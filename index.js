0269849a6251c357e2b4670fbaee61a402f23ee3
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-20 15:25:33 +1000

    download a single index.html file and save it to disk

---
+++ index.js
@@ -0,0 +1,119 @@
+"use strict";
+
+// Node.js built-ins
+
+var EventEmitter = require("events").EventEmitter;
+
+var crypto = require("crypto");
+var fs = require("fs");
+var path = require("path");
+var url = require("url");
+
+// 3rd-party modules
+
+var mkdirp = require("mkdirp");
+var request = require("request");
+var temp = require("temp").track();
+
+// this module
+
+/**
+ * @constructor
+ * @param {Object} opts { remoteUrl: "", localPath: "" }
+ */
+function Fetcher(opts) {
+  this.date = new Date();
+  this.remoteUrl = opts.remoteUrl;
+  this.localPath = opts.localPath;
+  this.tempPath = "";
+}
+
+Fetcher.prototype = Object.create(EventEmitter.prototype);
+Fetcher.prototype.constructor = Fetcher;
+
+Fetcher.prototype.afterTempPath = function () {
+  var me = this;
+
+  return new Promise(function (resolve, reject) {
+    temp.mkdir("appcache-fetcher-" + me.date.valueOf(), function (err, dirPath) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      me.tempPath = dirPath;
+      resolve(dirPath);
+    });
+  });
+};
+
+Fetcher.prototype.afterLocalPath = function () {
+  var me = this;
+
+  return new Promise(function (resolve, reject) {
+    mkdirp(me.localPath, function (err) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(me.localPath);
+    });
+  });
+};
+
+Fetcher.prototype.download = function (remoteUrl, localPath) {
+  var me = this;
+
+  return new Promise(function (resolve, reject) {
+    request({ url: remoteUrl }, function (reqErr, res, body) {
+      var filePath;
+      if (reqErr) {
+        console.error(reqErr);
+        reject(reqErr);
+        return;
+      }
+      if (res.statusCode !== 200) {
+        console.error("http statusCode: " + res.statusCode);
+        reject(new Error("http statusCode: " + res.statusCode));
+        return;
+      }
+      filePath = path.join(localPath, me.generateLocalFilePath(remoteUrl));
+      fs.writeFile(filePath, body, function (fsErr) {
+        if (fsErr) {
+          console.error(fsErr);
+          reject(fsErr);
+          return;
+        }
+        resolve(body);
+      });
+    });
+  });
+};
+
+Fetcher.prototype.generateLocalFilePath = function (remoteUrl) {
+  var parsed;
+  var hash;
+  if (remoteUrl === this.remoteUrl) {
+    return "index.html";
+  }
+  parsed = url.parse(remoteUrl, true, true);
+  hash = crypto.createHash("sha1");
+  hash.update(parsed.pathname + parsed.search);
+  return hash.digest("hex") + "." + path.extname(parsed.pathname);
+};
+
+Fetcher.prototype.go = function () {
+  var me = this;
+
+  return Promise.all([
+    this.afterTempPath(),
+    this.afterLocalPath()
+  ]).then(function () {
+    return me.download(me.remoteUrl, me.localPath);
+  });
+};
+
+Fetcher.extractManifestURL = function () {
+
+};
+
+module.exports = Fetcher;
---
+++ tests/index.js
@@ -0,0 +1,61 @@
+/*eslint-disable no-sync*/ // tests can be synchronous, relax!
+
+"use strict";
+
+// Node.js built-ins
+
+var fs = require("fs");
+var path = require("path");
+
+// 3rd-party modules
+
+var rimraf = require("rimraf");
+var test = require("tape");
+
+// our modules
+
+var Fetcher = require("..");
+
+// this module
+
+var outputPath = path.join(process.cwd(), "output");
+
+require("tape-chai");
+
+rimraf.sync(outputPath);
+
+test("Fetcher", function (t) {
+  t.isFunction(Fetcher);
+  t.end();
+});
+
+test("new Fetcher({ remoteUrl: 'http://everytimezone.com/' })", function (t) {
+  var fetcher;
+
+  t.test("constructor", function (tt) {
+    tt.doesNotThrow(function () {
+      fetcher = new Fetcher({
+        remoteUrl: "http://everytimezone.com",
+        localPath: outputPath
+      });
+    });
+    tt.ok(fetcher);
+    tt.end();
+  });
+
+  t.test(".go()", function (tt) {
+    fetcher.go().then(function () {
+      tt.ok(true);
+      tt.end();
+    }, function (err) {
+      tt.error(err);
+      tt.end();
+    });
+  });
+
+  t.test("index.html", function (tt) {
+    tt.ok(fs.existsSync(path.join(outputPath, "index.html")));
+    tt.end();
+  });
+
+});
