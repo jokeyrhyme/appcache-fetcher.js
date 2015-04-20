28dea7a5a80f0bd80347cd35d8d70505a1fe59bf
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-21 08:59:27 +1000

    split tests into different files

--- tests/index.js
+++ tests/index.js
@@ -4,12 +4,10 @@
 
 // Node.js built-ins
 
-var fs = require("fs");
 var path = require("path");
 
 // 3rd-party modules
 
-var cheerio = require("cheerio");
 var rimraf = require("rimraf");
 var test = require("tape");
 
@@ -30,65 +28,4 @@ test("Fetcher", function (t) {
   t.end();
 });
 
-test("new Fetcher({ remoteUrl: 'http://everytimezone.com/' })", function (t) {
-  var fetcher;
-
-  t.test("constructor", function (tt) {
-    tt.doesNotThrow(function () {
-      fetcher = new Fetcher({
-        remoteUrl: "http://everytimezone.com/",
-        localPath: outputPath
-      });
-    });
-    tt.ok(fetcher);
-    tt.end();
-  });
-
-  t.test(".go()", function (tt) {
-    fetcher.go().then(function () {
-      tt.ok(true);
-      tt.end();
-    }, function (err) {
-      tt.error(err);
-      tt.end();
-    });
-  });
-
-  t.test("index.html", function (tt) {
-    var contents;
-    var $;
-    tt.ok(fs.existsSync(path.join(outputPath, "index.html")));
-    contents = fs.readFileSync(path.join(outputPath, "index.html"), { encoding: "utf8" });
-    $ = cheerio.load(contents);
-    tt.notOk($("html").attr("manifest"), "no AppCache manifest attribute");
-    tt.end();
-  });
-
-  t.test("index.json", function (tt) {
-    var index;
-    tt.ok(fs.existsSync(path.join(outputPath, "index.json")));
-    tt.doesNotThrow(function () {
-      index = require(path.join(outputPath, "index.json"));
-    });
-    tt.isObject(index);
-    tt.equal(index["http://everytimezone.com/"], "index.html");
-    tt.end();
-  });
-
-  t.test("appcache.manifest", function (tt) {
-    tt.ok(fs.existsSync(path.join(outputPath, "appcache.manifest")));
-    tt.end();
-  });
-
-  t.test("appcache.json", function (tt) {
-    var appCache;
-    tt.ok(fs.existsSync(path.join(outputPath, "appcache.json")));
-    tt.doesNotThrow(function () {
-      appCache = require(path.join(outputPath, "appcache.json"));
-    });
-    tt.isObject(appCache);
-    tt.isArray(appCache.cache);
-    tt.end();
-  });
-
-});
+require("./01_everytimezone");
