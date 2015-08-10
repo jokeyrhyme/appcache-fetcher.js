a52a290b869c86fb057f6659eafde0919f18b0e2
Maggie Shi <maggie@blinkmobile.com.au>
2015-08-10 11:12:31 +1000

    remove console log

--- www/shims/jquery.html.js
+++ www/shims/jquery.html.js
@@ -2,24 +2,20 @@
 
 /**
  * @param {FetcherIndex} index to use for URL resolution
- * @param {Object} context e.g. jQuery, $, etc
- * @param {String} method name e.g. 'ajax'
+ * @param {Object} context e.g. jQuery, $.fn, etc
+ * @param {String} method name e.g. 'html'
  * @returns {Function} the shim
  */
-
-//var cheerio = require('cheerio');
 
 module.exports = function (index, context, method) {
   var oldMethod = context[method];
 
   var fixSrc = function (whole, value) {
-    console.log('input whole is: '+ whole + ', value is: ' + value);
     var fixValue;
     if (value && typeof value === 'string') {
       fixValue = index.resolveLocalUrl(value);
       whole = whole.replace(value, fixValue);
     }
-    console.log('fixed whole is: '+ whole + ', value is: ' + fixValue);
     return whole;
   };
 
